c89bf687792a142b4810bd4f6a958acce2b9eac6
anton-p-ivanov <anton.p.ivanov@gmail.com>
2019-01-21 17:08:48 +0300

    Downloads

--- assets/js/widget.js
+++ assets/js/widget.js
@@ -1,22 +1,36 @@
 (function($){
 
+    let widgets = [];
+    let request = function (widget) {
+        $.ajax({
+            url: widget.data('url'),
+            headers: {
+                'Widget-Request-Url': widget.data('endpoint'),
+                'Widget-Search-Conditions': JSON.stringify(widget.data('conditions')),
+                'Widget-Template': widget.data('template')
+            },
+            success: function (response) {
+                widget.replaceWith(response);
+            },
+            complete: function () {
+                let widget = widgets.shift();
+                if (widget) {
+                    request(widget);
+                }
+            },
+        });
+    };
+
     $.fn.widget = function() {
 
         this.each(function () {
-            let widget = $(this);
-
-            $.ajax({
-                url: widget.data('url'),
-                headers: {
-                    'Widget-Request-Url': widget.data('endpoint'),
-                    'Widget-Search-Conditions': JSON.stringify(widget.data('conditions')),
-                    'Widget-Template': widget.data('template')
-                },
-                success: function (response) {
-                    widget.replaceWith(response);
-                }
-            });
+            widgets.push($(this));
         });
+
+        let widget = widgets.shift();
+        if (widget) {
+            request(widget);
+        }
 
         return this;
     };
--- src/Controller/Support/DownloadsController.php
+++ src/Controller/Support/DownloadsController.php
@@ -6,6 +6,7 @@ use App\Repository\Repository;
 use App\Resource\Catalog\Element;
 use App\Resource\Storage\Storage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
